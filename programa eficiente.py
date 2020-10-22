60b2bd06d7f0fd2b4c37d80a74b423485b718096
Hatziul <72427131+Hatziul@users.noreply.github.com>
2020-10-22 14:52:01 -0500

    Delete programa eficiente.py

--- programa eficiente.py
+++
@@ -1,7 +0,0 @@
-#programa eficiente
-
-numero = int (input('inserte un numero entero:   '))
-
-for i in range(1,11):
-    resultado=i*numero
-    print(("%d  x  %d    =   %d")  %   (numero,i,resultado))
